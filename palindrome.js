5b568d05559b5f55f9c9e50a086438147c8ed946
Sarasmishra <sarasmishra8100@gmail.com>
2023-10-10 18:35:52 +0530

    added palindrome file

---
+++ palindrome.js
@@ -0,0 +1,12 @@
+let name = "saras";
+
+let bag ="";
+for(let i=name.length-1;i>=0;i--){
+    bag+=name[i];
+}
+if(name==bag){
+    console.log("palindrome")
+}
+else{
+    console.log("Not a Palindrome")
+}
